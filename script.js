63236be735bbea4e61f71b1c4a4cf0b56b5e9dac
karpagalakshmi.p <karpagalakshmippandian@gmail.com>
2020-08-28 21:14:09 +0530

    commit v3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 app.controller('MainCtrl', function($scope) {
 $scope.add=function(num1,num2)
 {
-  $scope.result=parseInt(num1)+parseInt(num2);
+  $scope.result=parseInt(num1)-parseInt(num2);
 }
 
 });
